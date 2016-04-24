a0e448d177721be14dab53ba11839aff638071d0
Yukihisa Kurimoto <ykurigit@gmail.com>
2016-04-24 22:40:47 +0900

    add typescript

--- js/js.js
+++ js/js.js
@@ -1,4 +1,28 @@
 "use strict";
+
+function article_ani() {
+  var ar = document.querySelector('.articles');
+  if (ar) {
+    ar.classList.remove('ani');
+  }
+}
+
+function hide_new(){
+  var es = document.querySelectorAll('span.new');
+  if (es&&es.length) {
+    var nowS = Math.floor(Date.now()/1000), i, len, e, time;
+    for (i=0,len=es.length;i<len;++i){
+      e = es[i];
+      time = parseInt(e.getAttribute('data-time'), 10);
+      console.log('nows:'+nowS+' time:'+time+' diff:'+(nowS-time));
+      if ((nowS-time) > 432000) {
+      //if ((nowS-time) > (-100000)) {
+        e.style.display='none';
+      }
+    }
+  }
+}
+
 /** 
  main
 */
@@ -10,4 +34,6 @@ function stickycb(sticky, visible) {
 
 document.addEventListener('DOMContentLoaded',function(){
   new Sticky('sticky', stickycb);
+  hide_new();
+  article_ani();
 });
